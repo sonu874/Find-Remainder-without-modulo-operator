4320d7903996077ee5b24be820132769c50f4bd9
Soumyadeep Das <72145432+sonu874@users.noreply.github.com>
2022-06-27 12:09:16 +0530

    Update bitwise_remainder.py

--- bitwise_remainder.py
+++ bitwise_remainder.py
@@ -4,8 +4,8 @@ def bit_div(a,b):
 
     neg = a < 0 or b < 0 # Checking if one of the numbers is negative
 
-    a = abs(a) # making sure both the numbers
-    b = abs(b) # are positive
+    a = abs(a) # making sure both the numbers are positive
+    b = abs(b) 
 
     for i in range(31,-1,-1): # starting our loop
 
@@ -13,9 +13,9 @@ def bit_div(a,b):
             a -= b << i   # subtracting b << i from a
             ans += 1 << i # adding 2 power i to the answer
 
-    # and finally checking if the output should be negative and returning it
-    #return ans if neg == 0 else -1 * ans
-    return a & ~((~int(0)) << b)
+    
+    #(return ans if neg == 0 else -1 * ans) # finally checking if the output should be negative and returning it after turning it into positive. If positive returning the same value.
+    return a & ~((~int(0)) << b) # returning the remainder after division
     
 #main
 print(bit_div(35,3))
